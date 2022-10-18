fe0d4f3f03b9068ef3552d586e7060f5e84d441e
Atnón López <anton.lopez.nunez@udc.es>
2022-10-18 11:01:51 +0200

    Added P1 files

---
+++ p1/expr2.ml
@@ -0,0 +1,5 @@
+let u = 1 + 4 * 2 - 4 / 2;; (*resultado: 7*)
+let v = asin 1.;; (*resultado: 1.57*)
+let w = char_of_int 10;; (*resultado: '\n'*)
+let x = asin 10.0 = 25.0 +. 17.0;; (*resultado: false*)
+let y = if 1 = 3 then "1 es igual a 3" else "1 es distinto de 3";;
---
+++ p1/nombre.ml
@@ -0,0 +1,3 @@
+print_endline "Anton Lopez Nunez";;
+print_endline "anton.lopez.nunez@udc.es";;
+(*podriamos usar print_string "" pero no imprime el salto de linea*)
